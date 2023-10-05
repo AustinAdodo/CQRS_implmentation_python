d643680eb8c8563b00c8df0e40e2a9ce74f384d7
Odia Adodo <subzbelow@gmail.com>
2023-10-05 14:26:07 +0100

    Updated ProductQueryHandler.py

--- product_management/ProductQueryHandler.py
+++ product_management/ProductQueryHandler.py
@@ -1,6 +1,10 @@
 class ProductQueryHandler:
-    def handle_get(self, query):
-        # Retrieve and return product data
-        pass
+    def __init__(self):
+        self.products = []
 
+    def handle_get(self, query):
+        for product in self.products:
+            if product.product_id == query.product_id:
+                return product
+        return None
     # Add handlers for other queries like GetAll, Search, etc.
